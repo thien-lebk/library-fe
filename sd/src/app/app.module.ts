b18fef801c2c96342ffc9d6d46472fd816bc8111
thien-lebk <thienlebk@gmail.com>
2021-08-06 09:27:23 +0700

    init

---
+++ sd/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LibraryComponent } from './modules/library/library.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LibraryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
